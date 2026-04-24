5a9a913c12868a229dd429de15ae51a9202ddca7
Suraz Agrahari <surazagrahari1980@gmail.com>
2026-04-24 14:24:13 +0545

    Learned importing and exporting modules.

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# nodeLearn
+# Going to learn node js fatrahi hai AI ki wajah se but that's alright
---
+++ node/app.js
@@ -0,0 +1,7 @@
+// const app = [1, 2, 3, 4, 5];
+// console.log(app);
+const { add, sub, mult } = require("./math");
+
+console.log(add(3, 4));
+console.log(sub(3, 4));
+console.log(mult(3, 4));
