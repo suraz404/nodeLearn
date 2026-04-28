89ef8fa0196adfa9556ec8c1b9297622e0418f7d
Suraz Agrahari <surazagrahari1980@gmail.com>
2026-04-28 11:11:58 +0545

    how to read file using fs module

--- node/node_suraz/syncFile.js
+++ node/node_suraz/syncFile.js
@@ -5,3 +5,6 @@ const fileName = "text.txt";
 const filePath = path.join(__dirname, fileName);
 
 fs.writeFileSync(filePath, "this is initial path", "utf-8");
+
+const fileRead = fs.readFileSync(filePath, "utf-8");
+console.log(fileRead);
