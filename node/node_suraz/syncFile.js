2616715352952e808b9681a46557cd5367b97759
Suraz Agrahari <surazagrahari1980@gmail.com>
2026-04-28 23:54:06 +0545

    feat: implement file operations including write, append, rename, and delete

--- node/node_suraz/syncFile.js
+++ node/node_suraz/syncFile.js
@@ -3,8 +3,15 @@ const path = require("path");
 
 const fileName = "text.txt";
 const filePath = path.join(__dirname, fileName);
+const newPath = path.join(__dirname, "updated.txt");
 
 fs.writeFileSync(filePath, "this is initial path", "utf-8");
 
 const fileRead = fs.readFileSync(filePath, "utf-8");
 console.log(fileRead);
+
+fs.appendFileSync(filePath, "This is updated path");
+
+fs.renameSync(filePath, newPath);
+
+fs.unlinkSync(); //this is how we are going to delete
