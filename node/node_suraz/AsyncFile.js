5e021daf0fc47b603423d327c6a34dbe819ce560
Suraz Agrahari <surazagrahari1980@gmail.com>
2026-04-30 14:55:07 +0545

    learned how to read using fs in async

--- node/node_suraz/AsyncFile.js
+++ node/node_suraz/AsyncFile.js
@@ -3,12 +3,20 @@ const fs = require("fs");
 
 const filePath = path.join(__dirname, "asyncFile.txt");
 
-fs.writeFile(
-  filePath,
-  "this is initial data using async file system",
-  "utf-8",
-  (err) => {
-    if (err) console.log("There is a error");
-    else console.log("file created ");
-  },
-);
+// fs.writeFile(
+//   filePath,
+//   "this is initial data using async file system",
+//   "utf-8",
+//   (err) => {
+//     if (err) console.log("There is a error");
+//     else console.log("file created ");
+//   },
+// );
+
+fs.readFile(filePath, "utf-8", (err, data) => {
+  if (err) {
+    console.log("there is error while fetching the data");
+  } else {
+    console.log(data);
+  }
+});
