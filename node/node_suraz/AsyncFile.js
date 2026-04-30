048e11e2b69bf9ad4060bf7e834203807228edd0
Suraz Agrahari <surazagrahari1980@gmail.com>
2026-04-30 23:13:25 +0545

    feat: add append and unlink functionality for async file operations

--- node/node_suraz/AsyncFile.js
+++ node/node_suraz/AsyncFile.js
@@ -3,15 +3,15 @@ const fs = require("fs");
 
 const filePath = path.join(__dirname, "asyncFile.txt");
 
-// fs.writeFile(
-//   filePath,
-//   "this is initial data using async file system",
-//   "utf-8",
-//   (err) => {
-//     if (err) console.log("There is a error");
-//     else console.log("file created ");
-//   },
-// );
+fs.writeFile(
+  filePath,
+  "this is initial data using async file system",
+  "utf-8",
+  (err) => {
+    if (err) console.log("There is a error");
+    else console.log("file created ");
+  },
+);
 
 fs.readFile(filePath, "utf-8", (err, data) => {
   if (err) {
@@ -20,3 +20,18 @@ fs.readFile(filePath, "utf-8", (err, data) => {
     console.log(data);
   }
 });
+
+fs.appendFile(
+  filePath,
+  "this is update data usinf async way",
+  "utf-8",
+  (err) => {
+    if (err) {
+      console.log("there is error");
+    } else {
+      console.log("It's done bro");
+    }
+  },
+);
+
+fs.unlink(filePath, "callback function");
